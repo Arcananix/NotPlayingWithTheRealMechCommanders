464e931a10618670109b970ac6b8f5d98d208718
Arcananix <43663274+Arcananix@users.noreply.github.com>
2018-12-31 17:46:35 -0600

    Add project files.

---
+++ NotPlayingWithTheRealMechCommanders/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Harmony;
+using System.Reflection;
+
+namespace NotPlayingWithTheRealMechCommanders
+{
+    public class Main
+    {
+        public static void Init(string _directory, string settings)
+        {
+            HarmonyInstance.Create("Arcananix.NotPlayingWithTheRealMechCommanders").PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ NotPlayingWithTheRealMechCommanders/MainMenu_Init_Patch.cs
@@ -0,0 +1,32 @@
+﻿using BattleTech;
+using BattleTech.UI;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+
+namespace NotPlayingWithTheRealMechCommanders
+{
+    [HarmonyPatch(typeof(MainMenu), "Init")]
+    public static class MainMenu_Init_Patch
+    {
+        private static void Prefix(MainMenu __instance)
+        {
+            try
+            {
+                FieldInfo field = __instance.GetType().GetField("topLevelMenu", BindingFlags.NonPublic | BindingFlags.Instance);
+                HBSRadioSet topLevelMenu = (HBSRadioSet)field.GetValue(__instance);
+                topLevelMenu.RadioButtons.Find((HBSButton x) => x.GetText() == "Career").gameObject.SetActive(false);
+                HBS.Logging.Logger.HandleUnityLog("NotPlayingWithTheRealMechCommanders - Remove Career button for offline Roguetech play", string.Empty, LogType.Log);
+            }
+            catch(Exception e)
+            {
+                HBS.Logging.Logger.HandleUnityLog("NotPlayingWithTheRealMechCommanders - Exception Removing Career button for offline Roguetech play", e.ToString(), LogType.Exception);
+            }
+        }
+    }
+}
